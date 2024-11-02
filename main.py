97c6759f85c2643630fdfb49d149d16ad80d1858
selivura <selivura@list.ru>
2024-11-02 13:45:08 +0500

    inital commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# TensorFlow and tf.keras
+import tensorflow as tf
+
+# Helper libraries
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
+
+class_names = ['Cat', 'Dog', 'git push -u origin main Parrot', 'Hamster', 'Fish']
